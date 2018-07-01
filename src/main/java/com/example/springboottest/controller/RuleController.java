b6bafff36019323d62f27e1098015f1213c731ef
souko2525 <yusuke.h0808@gmail.com>
2018-07-01 17:07:42 +0900

    fix bugs

--- src/main/java/com/example/springboottest/controller/RuleController.java
+++ src/main/java/com/example/springboottest/controller/RuleController.java
@@ -9,18 +9,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.springboottest.domain.entity.Rule;
 import com.example.springboottest.service.RuleService;
 
 @RestController
-@RequestMapping("/rule")
+@RequestMapping("api/V1/rule")
 public class RuleController {
+
     @Autowired
     RuleService ruleService;
+
     // 一覧表示
-    @RequestMapping(value="/selectall", method=RequestMethod.GET)
-    public List<Rule> ruleSelectAll() {
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public List<Rule> getRuleAll() {
         return ruleService.findAll();
+    }
+
+    @RequestMapping(method=RequestMethod.GET, value="{id}")
+    public Rule getRule(@PathVariable("id") Integer id) {
+        return ruleService.findById(id);
     }
 }
